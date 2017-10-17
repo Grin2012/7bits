761b3972998b34759c121cd8b8c05ed71cce943f
Grigoriy Putin <grigoriy.putin@gmail.com>
2017-10-17 22:04:41 +0600

    First commit

--- src/start/Start.java
+++ src/start/Start.java
@@ -3,6 +3,6 @@ package start;
 public class Start {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello my little java");
     }
 }
